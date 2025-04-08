447f4cc10858e35240ef27fdcd9c11f5514bc699
Rajan Dhiman <dhimanrajan7252@gmail.com>
2025-04-08 15:16:11 +0530

    Create sw.js

---
+++ tic-tac-toe/sw.js
@@ -0,0 +1,42 @@
+const CACHE_NAME = 'my-site-cache-v1';
+const URLS_TO_CACHE = [
+  '/', // your index.html
+  '/styles.css',
+  '/script.js',
+  '/logo.png'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(URLS_TO_CACHE);
+    })
+  );
+  console.log('[Service Worker] Installed');
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k))
+      )
+    )
+  );
+  console.log('[Service Worker] Activated');
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        return response || fetch(event.request).then(fetchRes => {
+          return caches.open(CACHE_NAME).then(cache => {
+            cache.put(event.request, fetchRes.clone());
+            return fetchRes;
+          });
+        });
+      })
+      .catch(() => caches.match('/')) // fallback
+  );
+});
